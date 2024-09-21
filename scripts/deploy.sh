70ac1523999cafd71ccc60b753b8527b2db6b1b0
Siddhant Gupta <siddhantgupta792000@gmail.com>
2024-09-21 18:14:42 +1000

    Initial commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,9 @@ az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenan
 echo "Retrieving AKS credentials..."
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER_NAME
 
+echo "Creating secret for image fetching..."
+kubectl create secret docker-registry acr-secret --docker-server=task9.azurecr.io --docker-username=$ACR_USERNAME --docker-password=$ACR_PASSWORD
+
 DEPLOYMENT_YAML="./scripts/kubernetes/deployment.yaml"
 
 echo "Deploying applications to Kubernetes..."
