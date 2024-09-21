63f93b49e13751285d4dac0c025e253df2299b67
Siddhant Gupta <siddhantgupta792000@gmail.com>
2024-09-21 16:15:50 +1000

    Initial commit

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -4,7 +4,7 @@ BOOK_IMAGE="task9.azurecr.io/book-catalog:latest"
 INVENTORY_IMAGE="task9.azurecr.io/inventory-management:latest"
 
 echo "Building book catalog image..."
-docker build -t $BOOK_IMAGE ../book_catalog
+docker build -t $BOOK_IMAGE ./book_catalog
 
 echo "Building inventory management image..."
-docker build -t $INVENTORY_IMAGE ../inventory_management
+docker build -t $INVENTORY_IMAGE ./inventory_management
