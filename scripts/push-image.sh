7e63724fedbffe8d660bac8755a66e4b691cb9a3
Siddhant Gupta <siddhantgupta792000@gmail.com>
2024-09-21 16:20:56 +1000

    Initial commit

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -3,11 +3,13 @@
 BOOK_IMAGE="task9.azurecr.io/book-catalog:latest"
 INVENTORY_IMAGE="task9.azurecr.io/inventory-management:latest"
 
-echo "Logging into Azure Container Registry..."
-az acr login --name task9
+echo "Logging into Azure Container Registry with Docker..."
+echo $ACR_PASSWORD | docker login task9.azurecr.io -u $ACR_USERNAME --password-stdin
 
 echo "Pushing book catalog image to ACR..."
 docker push $BOOK_IMAGE
 
 echo "Pushing inventory management image to ACR..."
 docker push $INVENTORY_IMAGE
+
+echo "Push completed successfully."
