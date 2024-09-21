d1e9af9f856a8953117e772b9a181e8fd6213820
Siddhant Gupta <siddhantgupta792000@gmail.com>
2024-09-21 16:00:57 +1000

    Initial commit

--- scripts/terraform/variables.tf
+++ scripts/terraform/variables.tf
@@ -17,27 +17,3 @@ variable "aks_name" {
 variable "dns_prefix" {
   default = "your-aks-dns-prefix"
 }
-
-variable "client_id" {
-  description = "Azure client ID"
-  type        = string
-  default     = ""
-}
-
-variable "client_secret" {
-  description = "Azure client secret"
-  type        = string
-  default     = ""
-}
-
-variable "subscription_id" {
-  description = "Azure subscription ID"
-  type        = string
-  default     = ""
-}
-
-variable "tenant_id" {
-  description = "Azure tenant ID"
-  type        = string
-  default     = ""
-}
