040b32d44db39965253940f964faecc1925a9a06
Siddhant Gupta <siddhantgupta792000@gmail.com>
2024-09-21 17:45:29 +1000

    Initial commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+RESOURCE_GROUP="task9-rg"
+AKS_CLUSTER_NAME="task9"
+
+echo "Retrieving AKS credentials..."
+az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER_NAME
+
 DEPLOYMENT_YAML="./scripts/kubernetes/deployment.yaml"
 
 echo "Deploying applications to Kubernetes..."
