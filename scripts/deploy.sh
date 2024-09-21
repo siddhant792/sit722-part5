63f9c6f5a913d33b87e9f5a9e2e2d5a4ae678ccb
Siddhant Gupta <siddhantgupta792000@gmail.com>
2024-09-21 17:55:44 +1000

    Initial commit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,9 @@
 RESOURCE_GROUP="task9-rg"
 AKS_CLUSTER_NAME="task9"
 
+echo "Logging into Azure..."
+az login --service-principal -u $AZURE_CLIENT_ID -p $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
+
 echo "Retrieving AKS credentials..."
 az aks get-credentials --resource-group $RESOURCE_GROUP --name $AKS_CLUSTER_NAME
 
